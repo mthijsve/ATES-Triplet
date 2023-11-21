b2a8dc56f112798f2738f490ddd42225bc65d6bd
mthijsve <muisdekogel@gmail.com>
2023-11-21 09:14:39 +0100

    regular changes/cleaning up

--- checktar.py
+++ checktar.py
@@ -1,4 +1,20 @@
 import tarfile
+import os
+import shutil
 
-tar = tarfile.open("output.tar.gz", "r:gz")
-tar.getmembers()
+# Define the path to the tar.gz file and the destination directory
+tar_file = 'Buoyancy2.tar.gz'
+dest_dir = 'buoyancy output'
+
+# Create the destination directory if it doesn't exist
+if not os.path.exists(dest_dir):
+    os.makedirs(dest_dir)
+
+# Open the tar.gz archive for reading
+with tarfile.open(tar_file, 'r:gz') as archive:
+    # Loop through all files in the archive
+    for member in archive.getmembers():
+        # Check if the file has a .csv extension
+        if member.name.endswith('.csv'):
+            # Extract the file to the destination directory
+            archive.extract(member, dest_dir)
